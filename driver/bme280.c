08688c34065ab629598552fd1db7d9ac8d7c2cdd
Ryan Lynch <lynchrl@colorado.edu>
2026-04-07 10:09:02 -0600

    Very basic miscdevice - loads/unloads with unbounded read with static string.

---
+++ driver/bme280.c
@@ -0,0 +1,58 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/uaccess.h>
+
+#define DEVICE_NAME "bme280"
+#define BME280_REG_CHIPID 0xD0
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ryan Lynch");
+MODULE_DESCRIPTION("BME280 I2C Misc Driver");
+
+static ssize_t bme280_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
+{
+    char out_str[64];
+    int len;
+
+    len = snprintf(out_str, sizeof(out_str), "Hello from BME280\n");
+    if (copy_to_user(buf, out_str, len))
+    {
+        return -EFAULT;
+    }
+
+    return len;
+}
+
+static const struct file_operations bme280_fops = {
+    .owner = THIS_MODULE,
+    .read = bme280_read,
+};
+
+static struct miscdevice bme280_miscdev = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = DEVICE_NAME, // This creates /dev/bme280
+    .fops = &bme280_fops,
+};
+
+static int __init bme280_init(void)
+{
+    pr_info("BME280: Loading driver...\n");
+    int ret = misc_register(&bme280_miscdev);
+    if (ret)
+    {
+        pr_err("BME280: Failed to register misc device\n");
+        return ret;
+    }
+    pr_info("BME280: Driver loaded.\n");
+    return 0;
+}
+
+static void __exit bme280_exit(void)
+{
+    misc_deregister(&bme280_miscdev);
+    pr_info("BME280: Driver unloaded.\n");
+}
+
+module_init(bme280_init);
+module_exit(bme280_exit);
