4146312c9dddef313f2b2a466a67b02453060a71
Ryan Lynch <rlynch5@gmail.com>
2026-04-14 14:01:40 -0600

    Add support to read/return pressure

--- driver/bme280.c
+++ driver/bme280.c
@@ -4,11 +4,11 @@
  * Description: A simple Linux kernel module to interface with the BME280 sensor over I2C on a Raspberry Pi.
  *
  * References used:
+ * - BME280 datasheet: https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
  * - Mutex guard: https://www.marcusfolkesson.se/blog/mutex-guards-in-the-linux-kernel/
  * - I2C client struct and related: https://elixir.bootlin.com/linux/v6.8/source/include/linux/i2c.h#L330
  * - https://github.com/raspberrypi/pico-examples/tree/master/i2c/bmp280_i2c
  * - https://github.com/raspberrypi/linux/blob/f76135166c099f776ed4dc4a94a073ffa9c2e1a4/drivers/iio/pressure/bmp280-i2c.c
- * - BME280 datasheet: https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
  */
 #include <linux/module.h>
 #include <linux/fs.h>
@@ -38,11 +38,9 @@ static s32 bme280_calc_t_fine(s32 adc_T, struct bme280_data *data)
     return var1 + var2;
 }
 
-static int read_calibrated_temp(struct bme280_data *data, s32 *temp)
+static int read_temp_adc(struct bme280_data *data, s32 *adc_T)
 {
     u8 raw_data[3];
-    s32 adc_T, t_fine;
-
     guard(mutex)(&bme280_mutex);
     if (!data || !data->client)
     {
@@ -55,23 +53,106 @@ static int read_calibrated_temp(struct bme280_data *data, s32 *temp)
         pr_err("BME280: Failed to read raw temperature data\n");
         return -EFAULT;
     }
-    adc_T = (raw_data[0] << 12) | (raw_data[1] << 4) | (raw_data[2] >> 4);
+    *adc_T = (raw_data[0] << 12) | (raw_data[1] << 4) | (raw_data[2] >> 4);
+    return 0;
+}
+
+static int read_temp(struct bme280_data *data, s32 *temp)
+{
+    s32 adc_T, t_fine;
+
+    if (read_temp_adc(data, &adc_T) < 0)
+    {
+        pr_err("BME280: Failed to read raw temperature data\n");
+        return -EFAULT;
+    }
     t_fine = bme280_calc_t_fine(adc_T, data);
     *temp = (t_fine * 5 + 128) >> 8;
     return 0;
 }
 
+static int read_pressure_adc(struct bme280_data *data, s32 *adc_P)
+{
+    u8 raw_data[3];
+    guard(mutex)(&bme280_mutex);
+    if (!data || !data->client)
+    {
+        pr_err("BME280: No I2C client available for pressure read\n");
+        return -ENODEV;
+    }
+
+    if (i2c_smbus_read_i2c_block_data(data->client, BME280_REG_PRESS_MSB, 3, raw_data) < 0)
+    {
+        pr_err("BME280: Failed to read raw pressure data\n");
+        return -EFAULT;
+    }
+    *adc_P = (raw_data[0] << 12) | (raw_data[1] << 4) | (raw_data[2] >> 4);
+    return 0;
+}
+
+// Section 4.2.3 in https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
+// Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format.
+static u32 bme280_calc_pressure(s32 adc_P, s32 t_fine, struct bme280_data *data)
+{
+    s64 var1, var2, p;
+
+    var1 = ((s64)t_fine) - 128000;
+    var2 = var1 * var1 * (s64)data->dig_P6;
+    var2 += ((var1 * (s64)data->dig_P5) << 17);
+    var2 += (((s64)data->dig_P4) << 35);
+    var1 = ((var1 * var1 * (s64)data->dig_P3) >> 8) + ((var1 * (s64)data->dig_P2) << 12);
+    var1 = (((((s64)1) << 47) + var1)) * ((s64)data->dig_P1) >> 33;
+
+    if (var1 == 0)
+    {
+        return 0; // Avoid division by zero
+    }
+    p = ((((s64)1048576 - (s32)adc_P) << 31) - var2) * 3125;
+    p = div64_s64(p, var1);
+    var1 = (((s64)data->dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+    var2 = ((s64)(data->dig_P8) * p) >> 19;
+    p = ((p + var1 + var2) >> 8) + (((s64)data->dig_P7) << 4);
+
+    return (u32)p;
+}
+
+static int read_pressure(struct bme280_data *data, s32 *pressure)
+{
+    s32 adc_P, adc_T, t_fine;
+    int ret;
+
+    ret = read_pressure_adc(data, &adc_P);
+    if (ret < 0)
+    {
+        pr_err("BME280: Failed to read raw pressure data\n");
+        return ret;
+    }
+
+    ret = read_temp_adc(data, &adc_T);
+    if (ret < 0)
+    {
+        pr_err("BME280: Failed to read raw temperature data\n");
+        return ret;
+    }
+    t_fine = bme280_calc_t_fine(adc_T, data);
+
+    *pressure = bme280_calc_pressure(adc_P, t_fine, data);
+    return 0;
+}
+
 static ssize_t bme280_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
 {
-    pr_info("BME280: Read called.\n");
     if (*offset != 0)
     {
         return 0; // EOF
     }
-    char ret_data[32];
-    int len, ret, t_int, t_frac;
+    char temp_string[32], pressure_string[32];
+    char output_string[64];
+    int len, total_len, ret, t_int, t_frac;
     s32 temp_calc;
-    ret = read_calibrated_temp(bme_data, &temp_calc);
+
+    // Read and format temperature.
+    ret = read_temp(bme_data, &temp_calc);
     if (ret < 0)
     {
         pr_err("BME280: Failed to read calibrated temperature\n");
@@ -83,25 +164,48 @@ static ssize_t bme280_read(struct file *file, char __user *buf, size_t count, lo
     // Handle negatives.
     if (temp_calc < 0 && t_int == 0)
     {
-        len = snprintf(ret_data, sizeof(ret_data), "T:-0.%02d\n", abs(t_frac));
+        len = snprintf(temp_string, sizeof(temp_string), "T:-0.%02d", abs(t_frac));
     }
     else
     {
-        len = snprintf(ret_data, sizeof(ret_data), "T:%d.%02d\n", t_int, abs(t_frac));
+        len = snprintf(temp_string, sizeof(temp_string), "T:%d.%02d", t_int, abs(t_frac));
     }
-
     if (len < 0)
     {
-        pr_err("BME280: Failed to format output string\n");
+        pr_err("BME280: Failed to format temperature string\n");
         return len;
     }
-    if (copy_to_user(buf, ret_data, len))
+
+    // Read and format pressure.
+    s32 pressure_calc;
+    ret = read_pressure(bme_data, &pressure_calc);
+    if (ret < 0)
+    {
+        pr_err("BME280: Failed to read calibrated pressure\n");
+        return ret;
+    }
+    pressure_calc = pressure_calc / 256; // Convert from Q24.8 to Pa
+    len = snprintf(pressure_string, sizeof(pressure_string), "P:%u.%02u", pressure_calc / 100, pressure_calc % 100);
+    if (len < 0)
+    {
+        pr_err("BME280: Failed to format pressure string\n");
+        return len;
+    }
+
+    total_len = snprintf(output_string, sizeof(output_string), "%s,%s\n", temp_string, pressure_string);
+    if (total_len < 0)
+    {
+        pr_err("BME280: Failed to format output string\n");
+        return total_len;
+    }
+
+    if (copy_to_user(buf, output_string, total_len))
     {
         pr_err("BME280: Failed to copy data to user space\n");
         return -EFAULT;
     }
-    *offset = len;
-    return len;
+    *offset = total_len;
+    return total_len;
 }
 
 static const struct file_operations bme280_fops = {
@@ -144,7 +248,18 @@ static int bme280_probe(struct i2c_client *client)
     bme_data->dig_T1 = i2c_smbus_read_word_data(client, BME280_REG_DIG_T1);
     bme_data->dig_T2 = i2c_smbus_read_word_data(client, BME280_REG_DIG_T2);
     bme_data->dig_T3 = i2c_smbus_read_word_data(client, BME280_REG_DIG_T3);
-    i2c_smbus_write_byte_data(client, BME280_REG_CTRL_MEAS, 0x23);
+    bme_data->dig_P1 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P1);
+    bme_data->dig_P2 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P2);
+    bme_data->dig_P3 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P3);
+    bme_data->dig_P4 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P4);
+    bme_data->dig_P5 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P5);
+    bme_data->dig_P6 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P6);
+    bme_data->dig_P7 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P7);
+    bme_data->dig_P8 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P8);
+    bme_data->dig_P9 = i2c_smbus_read_word_data(client, BME280_REG_DIG_P9);
+
+    // Enable normal mode, temp and pressure oversampling (section 3.2 in the datasheet).
+    i2c_smbus_write_byte_data(client, BME280_REG_CTRL_MEAS, 0x27);
     return 0;
 }
 
--- driver/bme280.h
+++ driver/bme280.h
@@ -1,22 +1,48 @@
-// Header for BME280 sensor driver/kernel module.
+/*
+ * Header for BME280 sensor driver/kernel module.
+ */
 
 #define DEVICE_NAME "bme280"
 
 #define BME280_REG_CHIPID 0xD0
 #define BME280_REG_CTRL_MEAS 0xF4
 #define BME280_REG_TEMP_MSB 0xFA
+#define BME280_REG_PRESS_MSB 0xF7
 
 // Calibration data registers
 #define BME280_REG_DIG_T1 0x88
 #define BME280_REG_DIG_T2 0x8A
 #define BME280_REG_DIG_T3 0x8C
 
+// Table 16 in https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
+#define BME280_REG_DIG_P1 0x8E
+#define BME280_REG_DIG_P2 0x90
+#define BME280_REG_DIG_P3 0x92
+#define BME280_REG_DIG_P4 0x94
+#define BME280_REG_DIG_P5 0x96
+#define BME280_REG_DIG_P6 0x98
+#define BME280_REG_DIG_P7 0x9A
+#define BME280_REG_DIG_P8 0x9C
+#define BME280_REG_DIG_P9 0x9E
+
 // Struct to hold calibration data, client reference, and other necessary information.
 struct bme280_data
 {
     struct i2c_client *client;
-    // Table 16 in https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
+    // Compensation values. See Table 16 in
+    // https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
+    // Temperature
     u16 dig_T1;
     s16 dig_T2;
     s16 dig_T3;
+    // Pressure
+    u16 dig_P1;
+    s16 dig_P2;
+    s16 dig_P3;
+    s16 dig_P4;
+    s16 dig_P5;
+    s16 dig_P6;
+    s16 dig_P7;
+    s16 dig_P8;
+    s16 dig_P9;
 };
